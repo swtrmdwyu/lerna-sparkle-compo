f2797853ae0e76fa4167ceefb67da7fe25cdb0df
slwtrmdwy <caiquedimeu.pro@gmail.com>
2024-02-21 11:41:50 -0300

    arrumando erro

--- packages/sparkle-compo/src/App.tsx
+++ packages/sparkle-compo/src/App.tsx
@@ -4,11 +4,6 @@ import Note from "./components/Note";
 import SearchBar from "./components/SearchBar";
 
 function App() {
-  const note = {
-    title: 'sdkjfskdjf',
-    content: 'Lorem ipsum dolor sit amet. Qui rerum obcaecati ad consectetur blanditiis aut doloremque tempora aut iure optio eum quis animi. Est quisquam obcaecati non ipsam beatae qui internos eaque ab repellendus quia ad cupiditate voluptatem ea totam modi. Aut quam voluptatem qui impedit enim est quas numquam eos dolorum repellendus hic totam consequatur sit odit voluptatibus ab neque illo.',
-    date: '23 de jan'
-  }
   return (
     <>
     </>
